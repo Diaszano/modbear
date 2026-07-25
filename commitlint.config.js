95c37a03569a1c5f09086467b87cb08b78ef7044
Diaszano <61257292+Diaszano@users.noreply.github.com>
2026-07-25 03:59:53 -0300

    fix: ignore legacy merge commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  ignores: [(message) => message.startsWith("merge: ")],
   rules: {
     // Allow sentence-case subjects so Dependabot commits
     // ("bump X from Y to Z") pass validation.
